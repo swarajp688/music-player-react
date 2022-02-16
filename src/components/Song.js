0d5721f88fc6fdfb8665d3ae851e3651bd63fe85
swarajp688 <swarajp688@gmail.com>
2022-02-17 00:29:54 +0530

    added initial styling

---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+
+const Song = () => {
+  return (
+    <div className="song-container">
+        <h1>Picture</h1>
+        <h1>song name</h1>
+        <h1>artist</h1>
+    </div>
+  )
+}
+export default Song;
