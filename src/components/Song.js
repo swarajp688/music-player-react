ea7066248172ba0637fb303da991fd80f46f955b
swarajp688 <swarajp688@gmail.com>
2022-02-22 19:38:42 +0530

    final

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,7 +3,7 @@ const Song = ({currentSong ,isPlaying}) => {
     const {name , cover ,artist} = currentSong;
   return (
     <div className="song-container">
-        <img className={`${isPlaying ? 'rotate' : ''}`} alt={currentSong.name} src={cover} />
+        <img  alt={currentSong.name} src={cover} />
         <h2>{name}</h2>
         <h3>{artist}</h3>
     </div>
